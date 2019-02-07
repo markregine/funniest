0b0fbcba4a1bd65c257e120a1e4a5c217fd0ea59
mark_test <mark_test@CORP.AMAZONWORKSPACES.COM>
2019-02-07 15:58:51 -0500

    not working, but changed the file name to something unique for test pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+## this code was from https://docs.python.org/3/distutils/setupscript.html
+#from distutils.core import setup
+#
+#setup(name='funniest_1of1M',
+      #version='1.0',
+      #description='Funniest joke in the world',
+      #author='mark regine',
+      #author_email='mregine@betaxanalytics.com',
+      #url='http://github.com/markregine/funniest_1of1M/',
+      #packages=['funniest_1of1M'],
+     #)
 
-setup(name='funniest',
-      version='1.0',
-      description='Funniest joke in the world',
-      author='mark regine',
-      author_email='mregine@betaxanalytics.com',
-      url='http://github.com/markregine/funniest/',
-      packages=['funniest'],
-     )
+## this code from https://packaging.python.org/tutorials/packaging-projects/
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="funniest_1of1M-markregine",
+    version="0.0.1",
+    author="mark regine",
+    author_email="mregine@betaxanalytics.com",
+    description="Funniest joke in the world",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/markregine/funniest_1of1M/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+)
